74bdc62c8d7e73c36347b94b3c26a9474382f84a
kaish5536 <lighstar.wang@live.com>
2016-09-05 10:07:44 +0800

    ok0.1

---
+++ ascii.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import argparse
+
+IMG = 'zoro.jpg'
+WIDTH = 160
+HEIGHT = 80
+OUTPUT = 'zoro.txt'
+
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+
+# 将256灰度映射到70个字符上
+def get_char(r,g,b,alpha = 256):
+    if alpha == 0:
+        return ' '
+    length = len(ascii_char)
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+
+    unit = (256.0 + 1)/length
+    return ascii_char[int(gray/unit)]
+
+if __name__ == '__main__':
+
+	#改变图片大小
+    im = Image.open(IMG)
+    im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
+
+    txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j,i)))
+        txt += '\n'
+
+    print(txt)
+
+    #字符画输出到文件
+    if OUTPUT:
+        with open(OUTPUT,'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt",'w') as f:
+            f.write(txt)
